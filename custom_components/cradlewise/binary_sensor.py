b33b4938bb79963133dd6d1dc4d36ce4ca31e10a
Grantland Chew <grantlandchew@gmail.com>
2026-05-30 17:15:50 -0700

    refactor: remove rocking_not_effective and inside_* fields

--- custom_components/cradlewise/binary_sensor.py
+++ custom_components/cradlewise/binary_sensor.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Any
 
 from pycradlewise import CradlewiseCradle
 
@@ -22,7 +21,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN
 from .coordinator import CradlewiseCoordinator
 from .sensor import _device_info
 
@@ -71,24 +69,7 @@ BINARY_SENSOR_DESCRIPTIONS: tuple[CradlewiseBinarySensorEntityDescription, ...] 
         icon="mdi:lightbulb",
         value_fn=lambda c: c.light_on,
     ),
-    CradlewiseBinarySensorEntityDescription(
-        key="inside_sleep_schedule",
-        translation_key="inside_sleep_schedule",
-        icon="mdi:calendar-clock",
-        value_fn=lambda c: c.inside_sleep_schedule,
-    ),
-    CradlewiseBinarySensorEntityDescription(
-        key="inside_soothing_window",
-        translation_key="inside_soothing_window",
-        icon="mdi:clock-check",
-        value_fn=lambda c: c.inside_soothing_window,
-    ),
-    CradlewiseBinarySensorEntityDescription(
-        key="rocking_not_effective",
-        translation_key="rocking_not_effective",
-        icon="mdi:alert-outline",
-        value_fn=lambda c: c.rocking_not_effective,
-    ),
+
 )
 
 
